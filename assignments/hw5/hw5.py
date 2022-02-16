8e347627c531d5aee5dd8d8cdf489b9497f874c8
Christian Fowler <60233463+fowlerce@users.noreply.github.com>
2022-02-16 17:15:52 -0500

    Complete HW 5
    name_reverse() fails the test, but the actual output is the same as the expected output.
    thirds() give the correct output, but not on the same line? Maybe that's what is throwing an error and causing it to fail?

---
+++ assignments/hw5/hw5.py
@@ -0,0 +1,86 @@
+"""
+Name: Christian Fowler
+hw5.py
+
+Problem: Continues practice with strings and lists
+
+Certification of Authenticity:
+<include one of the following>
+I certify that this assignment is entirely my own work.
+"""
+
+def name_reverse():
+    name = str(input("Enter a name (first & last): "))
+    output = name.split(" ")
+    print(output[1], "\b,", output[0])
+
+def company_name():
+    url = input("Enter a domain")
+    parts = url.split(".")
+    print(parts[1])
+
+def initials():
+    students = eval(input("How many students are in the class?"))
+    count = 0
+    for i in range(students):
+        count = count + 1
+        name = input("What is the name of student {}{}".format(count, "?"))
+        name_split = name.split(" ")
+        first = name_split[0]
+        last = name_split[1]
+        first_initial = first[0]
+        last_initial = last[0]
+        print('{}{}'.format(first_initial, last_initial))
+
+def names():
+    names = input("Enter a list of names (first and last), separated by commas:")
+    splitter = names.split(" ")
+    for i in range(0, len(splitter), 2):
+        first = splitter[i]
+        first_initial = first[0]
+        last = splitter[i+1]
+        last_initial = last[0]
+        print('{}{}'.format(first_initial, last_initial))
+
+def thirds():
+    counter = 0
+    terms = eval(input("Enter the number of sentences:"))
+    for i in range(terms):
+        output = ''
+        counter = counter + 1
+        sentence = input("Enter Sentence {}:".format(counter))
+        for j in range(0, len(sentence), 3):
+            third = sentence[j]
+            output = output + third
+        print(output)
+
+def word_average():
+    sentence = input("Enter a sentence:")
+    splitter = sentence.split(" ")
+    sum = 0
+    for i in range(len(splitter)):
+        char_count = len(splitter[i])
+        sum = sum + char_count
+    avg = sum / len(splitter)
+    print(avg)
+
+def pig_latin():
+    sentence = input("Enter a sentence to convert to pig latin:")
+    splitter = sentence.split(" ")
+    for i in range(len(splitter)):
+        word = splitter[i]
+        shift = '{}{}{}'.format(word[1:], word[0], 'ay')
+        splitter[i] = shift
+        translated = ' '.join(splitter)
+    print(translated.lower())
+
+
+if __name__ == '__main__':
+    # name_reverse()
+    # company_name()
+    # initials()
+    # names()
+    # thirds()
+    # word_average()
+    # pig_latin()
+    pass
